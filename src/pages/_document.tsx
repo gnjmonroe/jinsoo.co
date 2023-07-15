c2488e6d0aeda9fa9dfd6994d4d9aa7488a12687
Greg Monroe <gnjmonroe@gmail.com>
2023-07-15 21:56:23 +0900

    fix: handle eslint errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,10 @@ export default function Document() {
         <meta name="description" content="I am a freelance frontend designer/developer based in Seoul." />
         
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" as="style" crossOrigin="" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css" />
+        <link rel="stylesheet"
+          as="style" 
+          crossOrigin=""
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css" />
       </Head>
       <body>
         <Main />
--- src/utils/handleDate.ts
+++ src/utils/handleDate.ts
@@ -1,4 +1,5 @@
 export default function handleDate(dateString: string): string {
   const [year, month] = dateString.split("-");
+
   return year === "ongoing" ? "ongoing" : `${year}/${month}`;
 }
