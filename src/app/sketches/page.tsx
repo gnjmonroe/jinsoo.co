2b0dfd57469c20526082b982cb88165d047ec7ee
Greg Monroe <gnjmonroe@gmail.com>
2024-06-17 21:05:21 +0900

    fix: remove toReversed
    Next/vercel does not support a polyfill for toReversed().

--- src/app/sketches/page.tsx
+++ src/app/sketches/page.tsx
@@ -3,13 +3,15 @@ import { getDirectories } from './utils';
 
 const sketchesPath = `./src/app/sketches`;
 const dirs = await getDirectories(sketchesPath);
-const dateFilenames = dirs.filter((dirname) => dirname !== 'utils');
+const dateFilenames = dirs
+  .filter((dirname) => dirname !== 'utils')
+  .sort((a, b) => b.localeCompare(a));
 
 const Sketches = () => (
   <main>
     <h2>sketches</h2>
     <ol>
-      {dateFilenames.toReversed().map((dfn) => (
+      {dateFilenames.map((dfn) => (
         <li key={dfn}>
           <a href={`./sketches/${dfn}`}>{dfn}</a>
         </li>
