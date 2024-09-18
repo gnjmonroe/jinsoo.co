2914c2d296710a0f248464dc054aee75749fad1a
Greg Monroe <gnjmonroe@gmail.com>
2024-09-18 17:10:30 +0900

    standardize styles between notes and sketch pages

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Header from '../components/Header';
 import { getDirectories } from '../sketches/utils';
 import * as styles from './page.css';
 
@@ -10,14 +11,17 @@ const dateFilenames = dirs.filter((dirname) => !dirsToSkip.includes(dirname));
 
 const NotesIndex = () => (
   <main className={styles.root}>
+    <Header />
     <h2>Notes</h2>
-    {dateFilenames
-      .sort((a, b) => b.localeCompare(a))
-      .map((dfn) => (
-        <Link className={styles.link} href={`notes/${dfn}`}>
-          {dfn}
-        </Link>
-      ))}
+    <ol className={styles.ol}>
+      {dateFilenames
+        .sort((a, b) => b.localeCompare(a))
+        .map((dfn) => (
+          <Link className={styles.link} href={`notes/${dfn}`}>
+            {dfn}
+          </Link>
+        ))}
+    </ol>
   </main>
 );
 
